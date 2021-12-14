663fb904869e2cc685cf933a5f178ebf7f4af1e9
musadevw <musadevweb@gmail.com>
2021-12-14 01:10:16 +0000

    deploy  avec server

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-// server.js
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const express = require('express')
+const cors = require('cors')
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-const port = process.env.port || 3000
+const app = express()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:true
+}))
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`Ready on ${port}`)
-  })
-})
+app.use(express.static('out'))
+app.listen(3000,()=>console.log('Reussire'))
